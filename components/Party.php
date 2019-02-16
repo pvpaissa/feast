58f840d3adbe2118c86e71c8baeb317deb2ecdb9
Paul Lovato <plovato@gmail.com>
2019-02-16 16:39:47 -0700

    Updated Party component class.

--- components/Party.php
+++ components/Party.php
@@ -3,6 +3,7 @@
 namespace Cleanse\Feast\Components;
 
 use Config;
+use ValidationException;
 use Cms\Classes\ComponentBase;
 use Cleanse\Feast\Models\FeastParty;
 
@@ -15,39 +16,38 @@ class Party extends ComponentBase
     public function componentDetails()
     {
         return [
-            'name'            => 'Overall Party Feast Rankings',
-            'description'     => 'Grabs the rankings for feast party players.'
+            'name'          => 'Overall Party Feast Rankings',
+            'description'   => 'Grabs the rankings for feast party players.'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [
+            'season' => [
+                'title'       => 'Feast Season',
+                'description' => 'If you wish to specify a specific season.',
+                'default'     => Config::get('cleanse.feast::season', 1),
+                'type'        => 'string',
+                'validationPattern' => '^[0-9]+$',
+                'validationMessage' => 'Please enter a season number.'
+            ]
         ];
     }
 
     public function onRun()
     {
-        $this->season = $this->page['season'] = Config::get('cleanse.feast::season', 1);
         $this->amount = Config::get('cleanse.feast::party_full_take', 50);
+
+        $this->season = $this->page['season'] = $this->property('season');
 
         $this->rankings = $this->page['rankings'] = $this->loadRankings();
     }
 
-    public function loadRankings()
+    private function loadRankings()
     {
         return FeastParty::where('season', $this->season)
             ->orderBy('rating', 'desc')
             ->paginate($this->amount);
-    }
-
-    public function onServer()
-    {
-        $server = post('newItem', 'Balmung');
-
-        if ($server != '') {
-            $server = FeastParty::whereHas('player', function($q) use ($server){
-                $q->where('name', 'like', '%'.$server.'%');
-            })
-                ->where('season', $this->season)
-                ->orderBy('rating', 'desc')
-                ->get();
-        }
-
-        $this->page['items'] = $server;
     }
 }
