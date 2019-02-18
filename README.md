76ef531115aae48c99b9840de3e95df745632209
Paul Lovato <plovato@gmail.com>
2019-02-17 17:44:50 -0700

    Install/New Season. Not final.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PvPaissa Feast Rankings
 A custom plugin for the PvPaissa website.
+
+### Todo:
+- Add 'season' dropdown for the person's feast profile.
+- Create backend component to handle ending a season / starting a new.
---
+++ components/Install.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Cleanse\Feast\Components;
+
+use Config;
+use Log;
+use Queue;
+use Cms\Classes\ComponentBase;
+use Cleanse\Feast\Classes\FeastHelper;
+
+class Install extends ComponentBase
+{
+    public $test;
+
+    public function componentDetails()
+    {
+        return [
+            'name'          => 'PvPaissa Feast Installer.',
+            'description'   => 'Adds completed seasons to the database.'
+        ];
+    }
+
+    /**
+     * Look at frontlines updater. Refactor!
+     */
+    public function onRun()
+    {
+        $this->install();
+    }
+
+    public function install()
+    {
+        $feast = new FeastHelper;
+        $day = $feast->yearDay();
+        $tiers = $feast->tiers;
+        $types = $feast->types;
+        $datacenters = $feast->datacenters;
+        $season = Config::get('cleanse.feast::season', 2) - 1; //todo
+
+        Log::info('Starting feast install.');
+
+        foreach ($types as $type) {
+            foreach ($datacenters as $datacenter) {
+                foreach ($tiers as $tier) {
+                    $data = [
+                        'datacenter' => $datacenter,
+                        'day' => $day,
+                        'tier' => $tier,
+                        'type' => $type,
+                        'season' => $season
+                    ];
+
+                    if ($type === 'party' and $tier <= 4) {
+                        //Log::info('Skipping: '.$datacenter.' '.$type.' '.$tier);
+                    } else {
+                        //Log::info('else '.$datacenter.' '.$type.' '.$tier.' '.$season);
+                        Queue::push('\Cleanse\Feast\Classes\Jobs\ScrapeFeast', $data);
+                    }
+                }
+            }
+
+            Queue::push('\Cleanse\Feast\Classes\Jobs\RankFeastDaily', [
+                'day' => $day,
+                'type' => $type,
+                'season' => $season
+            ]);
+        }
+    }
+}
